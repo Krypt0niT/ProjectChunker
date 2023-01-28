f163af0f991498bbe4b087234632efa9c6be1135
Krypt0niT <k269692@gmail.com>
2023-01-28 17:09:28 +0100

    test base

--- ProjectChunker/Assets/Scripts/Player.cs
+++ ProjectChunker/Assets/Scripts/Player.cs
@@ -11,11 +11,13 @@ public class Player : MonoBehaviour
     public float jumpForce = 10;
     bool grounded = false;
     bool jumpReset = true;
+    Transform WeaponHolder;
     // Start is called before the first frame update
     void Start()
     {
         save = GameObject.Find("Manager").GetComponent<Save>();
         rb = GetComponent<Rigidbody2D>();
+        WeaponHolder = GameObject.Find("WeaponHolder").transform;
     }
 
     // Update is called once per frame
@@ -23,29 +25,12 @@ public class Player : MonoBehaviour
     {
         PlayerMove();
     }
-
-    void PlayerMove()
+    private void Update()
     {
-        float x;
-
-        float y = 0;
-
-        if (Input.GetKey(save.player.moveLeft) && !Input.GetKey(save.player.moveRight)) {x = -1; }
-        else if(!Input.GetKey(save.player.moveLeft) && Input.GetKey(save.player.moveRight)) {x = 1; }
-        else {x = 0; }
-
-        if (Input.GetKey(save.player.jump) && grounded && jumpReset)
-        {
-            y = jumpForce;
-            print("jump");
-            jumpReset = false;
-
-            
-        }
-        //print(rb.velocity.y + " "+ y);
-        moveDirection = new Vector2(x * speed, rb.velocity.y + y);
-        rb.velocity = moveDirection;
+        weapon();
     }
+
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -61,5 +46,32 @@ public class Player : MonoBehaviour
             grounded = false;
             jumpReset = true;
         }
+    }
+    void PlayerMove()
+    {
+        float x;
+
+        float y = 0;
+
+        if (Input.GetKey(save.player.moveLeft) && !Input.GetKey(save.player.moveRight)) { x = -1; }
+        else if (!Input.GetKey(save.player.moveLeft) && Input.GetKey(save.player.moveRight)) { x = 1; }
+        else { x = 0; }
+
+        if (Input.GetKey(save.player.jump) && grounded && jumpReset)
+        {
+            y = jumpForce;
+            jumpReset = false;
+
+
+        }
+        moveDirection = new Vector2(x * speed, rb.velocity.y + y);
+        rb.velocity = moveDirection;
+
+    }
+    void weapon()
+    {
+        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(new Vector3(WeaponHolder.position.x, WeaponHolder.position.y, WeaponHolder.position.z));
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        WeaponHolder.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
