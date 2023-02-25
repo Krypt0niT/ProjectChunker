ed0f96ff4f6a303fc0c1d600b2a14f5308da36f7
Krypt0niT <k269692@gmail.com>
2023-02-26 00:38:38 +0100

    blokady poschodia

--- ProjectChunker/Assets/Scripts/CameraFollow.cs
+++ ProjectChunker/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed;
     public Vector3 offset;
 
     private void LateUpdate()
